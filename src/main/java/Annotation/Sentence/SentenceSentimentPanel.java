35ef6d315bc2f3438e5f66aa4aee25d1d1c03b46
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2022-01-10 12:51:16 +0300

    Added abstract methods for generalization of drawing panel.

--- src/main/java/Annotation/Sentence/SentenceSentimentPanel.java
+++ src/main/java/Annotation/Sentence/SentenceSentimentPanel.java
@@ -6,12 +6,43 @@ import AnnotatedSentence.ViewLayerType;
 import DataCollector.Sentence.SentenceAnnotatorPanel;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class SentenceSentimentPanel extends SentenceAnnotatorPanel {
 
     public SentenceSentimentPanel(String currentPath, String fileName) {
         super(currentPath, fileName, ViewLayerType.POLARITY);
         setLayout(new BorderLayout());
+    }
+
+    @Override
+    protected void setWordLayer() {
+        clickedWord.setPolarity(list.getSelectedValue().toString());
+    }
+
+    @Override
+    protected void setBounds() {
+        pane.setBounds(((AnnotatedWord)sentence.getWord(selectedWordIndex)).getArea().x, ((AnnotatedWord)sentence.getWord(selectedWordIndex)).getArea().y + 20, 120, 80);
+    }
+
+    @Override
+    protected void drawLayer(AnnotatedWord word, Graphics g, int currentLeft, int lineIndex, int wordIndex, int maxSize, ArrayList<Integer> wordSize, ArrayList<Integer> wordTotal) {
+        if (word.getPolarity() != null){
+            String correct = word.getPolarity().toString();
+            g.drawString(correct, currentLeft, (lineIndex + 1) * lineSpace + 30);
+        }
+    }
+
+    @Override
+    protected int getMaxLayerLength(AnnotatedWord word, Graphics g) {
+        int maxSize = g.getFontMetrics().stringWidth(word.getName());
+        if (word.getPolarity() != null){
+            int size = g.getFontMetrics().stringWidth(word.getPolarity().toString());
+            if (size > maxSize){
+                maxSize = size;
+            }
+        }
+        return maxSize;
     }
 
     public int populateLeaf(AnnotatedSentence sentence, int wordIndex){
